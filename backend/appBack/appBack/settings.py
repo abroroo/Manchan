2791d03506da243a7e868895f5f97b54bfcbf575
Cyberspyde <52118091+cyberspyde@users.noreply.github.com>
2023-05-07 19:13:51 +0900

    key update

--- backend/appBack/appBack/settings.py
+++ backend/appBack/appBack/settings.py
@@ -11,16 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x*jb=osg^q$w0=%a6_1_1lsa73^37n!hzw+9!5(jrz_b7b0&!='
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
